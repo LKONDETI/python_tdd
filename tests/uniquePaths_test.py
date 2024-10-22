351ad2253b6db72f5d36f6a42f2abcc132370438
Sravani K <sravanikondeti4@gmail.com>
2024-10-22 17:55:56 -0400

    solved unique paths

---
+++ tests/uniquePaths_test.py
@@ -0,0 +1,16 @@
+from src.katas.uniquePaths import Solution
+from unittest import TestCase
+
+
+class TestUniquePaths(TestCase):
+
+    def test_example1(self):
+        m = 3
+        n = 7
+        solution = Solution()
+        self.assertEqual(solution.uniquePaths(m,n), 28)
+    
+    def test_example2(self):
+        
+        solution = Solution()
+        self.assertEqual(solution.uniquePaths(3,2), 3)
