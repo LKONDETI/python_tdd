e0dd42b43455b81cbf70a6040668f5dff9fc738a
Sravani K <sravanikondeti4@gmail.com>
2024-11-22 12:53:33 -0500

    worked on Valid Anagram

---
+++ tests/validAnagram_test.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from src.katas.validAnagram import Solution
+
+class TestFirstUniqueCharaterString(TestCase):
+
+    def test_anagram(self):
+        s = "anagram"
+        t = "nagaram"
+        result = Solution()
+        self.assertEqual(result.isAnagram(s,t), True)
+    
+    def test_car(self):
+        s = "car"
+        t = "rat"
+        result = Solution()
+        self.assertEqual(result.isAnagram(s,t), False)
