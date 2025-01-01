096bdaba6f657689ba32827954e654f2fe2be0f5
Sravani K <sravanikondeti4@gmail.com>
2025-01-01 23:57:54 +0530

    worked on the test case in reverse linked list

--- tests/reverseLinkedList_test.py
+++ tests/reverseLinkedList_test.py
@@ -12,7 +12,7 @@ class TestReverseLinkedList(TestCase):
         head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
         expected = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
         result = Solution().reverseList(head)
-        self.assertTrue(self.compare_lists(result, expected))
+        self.assertEqual(self.compare_lists(result, expected), True)
     
 
     def test_double_node_list(self):
@@ -20,13 +20,13 @@ class TestReverseLinkedList(TestCase):
         expected = ListNode(2, ListNode(1))
         result = Solution().reverseList(head)
         self.assertTrue(self.compare_lists(result, expected))
-    
+    '''
     def compare_lists(self, l1, l2):
         while l1 and l2:
             if l1.val != l2.val:
                 return False
             l1 = l1.next
             l2 = l2.next
-        return l1 is None and l2 is None
+        return l1 is None and l2 is None'''
     
     
