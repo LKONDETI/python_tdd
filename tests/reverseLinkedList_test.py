e00093df4b2ef2f9bdc1e976b0d3f0e05ea91f45
Sravani K <sravanikondeti4@gmail.com>
2025-02-05 18:50:24 -0500

    solved reverse Linked list 2

--- tests/reverseLinkedList_test.py
+++ tests/reverseLinkedList_test.py
@@ -20,13 +20,13 @@ class TestReverseLinkedList(TestCase):
         expected = ListNode(2, ListNode(1))
         result = Solution().reverseList(head)
         self.assertTrue(self.compare_lists(result, expected))
-    '''
+    
     def compare_lists(self, l1, l2):
         while l1 and l2:
             if l1.val != l2.val:
                 return False
             l1 = l1.next
             l2 = l2.next
-        return l1 is None and l2 is None'''
+        return l1 is None and l2 is None
     
     
