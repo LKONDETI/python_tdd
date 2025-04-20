d21125d7665857a14a9501c1194ef23e1c28e668
Sravani K <sravanikondeti4@gmail.com>
2025-04-19 23:42:03 -0400

    added two test cases for the rotate array

--- tests/rotateElement_test.py
+++ tests/rotateElement_test.py
@@ -10,3 +10,17 @@ class TestRotateArray(TestCase):
         result = Solution()
         self.assertEqual(result.rotate(nums,k), output)
     
+    def test_example_2(self):
+        nums = [-1,-100,3,99]
+        k = 2
+        output = [3,99,-1,-100]
+        result = Solution()
+        self.assertEqual(result.rotate(nums,k), output)
+    
+    def test_two_items(self):
+        nums = [1,2]
+        k = 3
+        output = [2,1]
+        result = Solution()
+        self.assertEqual(result.rotate(nums,k), output)
+    
