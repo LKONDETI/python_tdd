ddf34978ef5b5e83fb309ca80f4723e1e29d5c3c
Sravani K <sravanikondeti4@gmail.com>
2026-01-02 23:57:40 -0500

    solved dota 2 senate

---
+++ src/katas/dota2Senate.py
@@ -0,0 +1,15 @@
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        R = deque(i for i, p in enumerate(senate) if p == 'R')
+        D = deque(i for i, p in enumerate(senate) if p == 'D')
+
+        while R and D:
+            radiant, dire = R.popleft(), D.popleft()
+
+            if radiant < dire:
+                R.append(radiant + len(senate))
+            else:
+                D.append(dire + len(senate))
+            
+        return "Radiant" if R else "Dire"  
